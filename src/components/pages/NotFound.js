1fbdb43105cf6730721f1beef20fa1de65def328
Hameed Jimoh <hameed.jimoh1601@gmail.com>
2021-05-03 06:03:14 -0700

    Added a react icon to the not found page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import { BsTools } from "react-icons/bs";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 import safewashGallon from "../../assets/bq-safewash-only-1.png";
@@ -14,12 +16,29 @@ const useStyles = makeStyles((theme) => ({
     backgroundRepeat: "no-repeat",
     backgroundPosition: "bottom",
     backgroundSize: "cover",
+    padding: "0 10em",
+
+    [theme.breakpoints.down("md")]: {
+      padding: "0 5em",
+    },
+
+    [theme.breakpoints.down("sm")]: {
+      padding: "5em",
+    },
+
+    [theme.breakpoints.down("xs")]: {
+      padding: "5em 2em",
+    },
   },
   primaryHeading: {
     fontFamily: "Open Sans",
+
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "2rem",
+    },
   },
   secondaryHeading: {
-    fontWeight: 700,
+    fontWeight: 600,
     color: theme.palette.common.brown,
   },
   divider: {
@@ -49,50 +68,66 @@ const useStyles = makeStyles((theme) => ({
 const NotFound = () => {
   const classes = useStyles();
   const theme = useTheme();
+
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Grid
       container
-      direction="column"
       className={classes.parentGrid}
-      justify="center"
+      direction={matchesSM ? "column" : "row"}
+      justify={matchesSM ? "center" : "space-evenly"}
       alignItems="center"
     >
-      <Grid item>
-        <Typography variant="h1" className={classes.primaryHeading}>
-          Oops!
-        </Typography>
-      </Grid>
-      <Grid item style={{ marginTop: ".5em", marginBottom: ".75em" }}>
-        <Typography
-          variant="h6"
-          className={classes.secondaryHeading}
-          align="center"
-        >
-          It looks like the page you are looking for is broken or does not
-          exist.
-        </Typography>
+      <Grid item style={{ marginBottom: matchesSM ? "3em" : undefined }}>
+        <BsTools size={matchesXS ? 100 : matchesSM ? 150 : 200} color="#ccc" />
       </Grid>
       <Grid item>
-        <Typography
-          variant="body1"
-          align="center"
-          style={{ color: theme.palette.common.brown }}
+        <Grid
+          container
+          direction="column"
+          alignItems={matchesSM ? "center" : "flex-start"}
         >
-          Do not panic. Just click on the button below to return to the homepage
-        </Typography>
-      </Grid>
-      <Grid item style={{ marginTop: ".75em", marginBottom: "1.5em" }}>
-        <Divider className={classes.divider} />
-      </Grid>
-      <Grid item>
-        <Button
-          variant="contained"
-          className={classes.btn}
-          component={Link}
-          to="/"
-        >
-          Home
-        </Button>
+          <Grid item>
+            <Typography variant="h1" className={classes.primaryHeading}>
+              Oops!
+            </Typography>
+          </Grid>
+          <Grid item style={{ marginTop: ".5em", marginBottom: ".75em" }}>
+            <Typography
+              variant="h6"
+              className={classes.secondaryHeading}
+              align={matchesSM ? "center" : "left"}
+            >
+              It looks like the page you are looking for is broken or does not
+              exist.
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              align={matchesSM ? "center" : "left"}
+              style={{ color: theme.palette.common.brown }}
+            >
+              Do not panic. Just click on the button below to return to the
+              homepage
+            </Typography>
+          </Grid>
+          <Grid item style={{ marginTop: ".75em", marginBottom: "1.5em" }}>
+            <Divider className={classes.divider} />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              className={classes.btn}
+              component={Link}
+              to="/"
+            >
+              Home
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
